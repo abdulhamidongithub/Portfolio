fd175fa146de12d75fa80c9f64aa62a711e76176
abdulhamidongithub <1997abdulhamid@gmail.com>
2024-12-11 20:27:21 +0900

    Homepage added

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -3,6 +3,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from mainapp.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Home.as_view(), name="home"),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
