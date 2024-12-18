59fd8603f128aee1650c0d1eb313eee91e2abd49
abdulhamidongithub <1997abdulhamid@gmail.com>
2024-12-19 00:50:46 +0900

    Finished AI Presentations page

--- mainapp/views.py
+++ mainapp/views.py
@@ -21,4 +21,22 @@ class ProjectView(View):
     def get(self, request):
         return render(request, "project.html")
 
+class AIApplications(View):
+    def get(self, request):
+        return render(request, "extra/ai_applications.html")
 
+class AIPPTs(View):
+    def get(self, request):
+        return render(request, "extra/ai_ppt.html")
+
+class AIImages(View):
+    def get(self, request):
+        return render(request, "extra/ai_images.html")
+
+class AIVideos(View):
+    def get(self, request):
+        return render(request, "extra/ai_video.html")
+
+class AIChatbots(View):
+    def get(self, request):
+        return render(request, "extra/ai_chatbot.html")
